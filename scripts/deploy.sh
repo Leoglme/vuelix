6593e3de70219a9e9a28deb6c61d1f1cfe5d5005
Léo glme <leo.guillaume@epitech.eu>
2021-11-18 16:21:32 +0100

    docs deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+GREEN='\033[0;32m'
 # abort on errors
 set -e
 
 # build
 yarn build
-
+printf "${GREEN} Docs build running...\n"
 cd dist
+
 
 yarn v
 npm publish
--- scripts/docDeploy.sh
+++ scripts/docDeploy.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+GREEN='\033[0;32m'
 # abort on errors
 set -e
 
 # build
 yarn docs:build
-
+printf "${GREEN} Docs build running...\n"
 cd docs/.vuepress/dist
 git add -A
 git commit -m 'docs deployment'
 git push
+printf "${GREEN} GIT push...\n"
