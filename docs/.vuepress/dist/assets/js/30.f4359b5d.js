524ef7c457f28179c92c1b098b68257cc6c0a8ef
Léo glme <leo.guillaume@epitech.eu>
2021-11-20 16:44:32 +0100

    docs build deploy

---
+++ docs/.vuepress/dist/assets/js/30.f4359b5d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{959:function(t,n,e){"use strict";e.r(n);var l={},o=e(0),s=Object(o.a)(l,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("vx-button",[this._v("Hello World")])],1)}),[],!1,null,null,null);n.default=s.exports}}]);
